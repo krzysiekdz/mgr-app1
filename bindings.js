00f9ca4a75ae03c0865a5ded3052faa7790384f6
Krzysztof Dziezyk <krzysztof.dziezyk@gmail.com>
2017-05-22 15:22:34 +0200

    bindings done

--- bindings.js
+++ bindings.js
@@ -4,7 +4,9 @@ module.exports = bind;
 
 bind.btn = {};
 bind.input = {};
+bind.checkbox = {};
 
+//add bindings
 bind.btn.addFirst = 'addFirst';
 bind.btn.addMid = 'addMid';
 bind.btn.addLast = 'addLast';
@@ -13,3 +15,40 @@ bind.btn.clear = 'clear';
 
 bind.input.add = 'input-add';
 bind.input.init = 'input-init';
+
+//update bindings
+bind.btn.updateFirst = 'updateFirst';
+bind.btn.updateMid = 'updateMid';
+bind.btn.updateLast = 'updateLast';
+bind.btn.updateEvery = 'updateEvery';
+
+bind.input.update = 'input-update';
+bind.input.update_every = 'input-update-every';
+
+//replace
+bind.btn.replaceFirst = 'replaceFirst';
+bind.btn.replaceMid = 'replaceMid';
+bind.btn.replaceLast = 'replaceLast';
+
+bind.input.replace = 'input-replace';
+
+//swap
+bind.btn.swapFirst = 'swapFirst';
+bind.btn.swapMid = 'swapMid';
+bind.btn.swapLast = 'swapLast';
+
+//fetch
+bind.btn.fetch_1k = 'fetch1';
+bind.btn.fetch_2k = 'fetch2';
+
+//input, edit
+bind.input.input = 'input-input';
+bind.input.edit = 'input-edit';
+bind.btn.edit = 'btn-edit';
+
+//filter
+bind.checkbox.filter = 'checkbox-filter';
+
+//search
+bind.btn.search = 'btn-search';
+bind.input.search = 'input-search';
